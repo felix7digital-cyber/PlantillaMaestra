37bfd724d3552b7a5dc16ec25e030a1a788fa5e5
felix7digital-cyber <felix7digital@gmail.com>
2026-08-18 19:03:24 -0400

    Create MainActivity with a welcome message

---
+++ app/src/main/java/com/plantilla/base/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.plantilla.base
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val textView = TextView(this).apply {
+            text = "¡Plantilla Maestra Lista para Producción!"
+            textSize = 22f
+            setPadding(40, 40, 40, 40)
+        }
+        
+        setContentView(textView)
+    }
+}
+
